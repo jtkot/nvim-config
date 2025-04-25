91e99c8a3070e7f30f5959bba63dd8fa84d57141
Jan Kot <janymalino@gmail.com>
2025-04-25 14:32:05 +0200

    init.lua: trim trailing whitespaces

--- init.lua
+++ init.lua
@@ -8,17 +8,17 @@ end
 
 local _, current_dir = lib.filesystem.self_path()
 
-local _, opts = lib.module.run(current_dir .. '/config/opts.lua') 
+local _, opts = lib.module.run(current_dir .. '/config/opts.lua')
 for k, v in pairs(opts) do
 	vim.o[k] = v
 end
 
-local _, cmds = lib.module.run(current_dir .. '/config/autostart.lua') 
+local _, cmds = lib.module.run(current_dir .. '/config/autostart.lua')
 for _, v in ipairs(cmds) do
 	vim.cmd(v)
 end
 
-local _, keymaps = lib.module.run(current_dir .. '/config/keymaps.lua') 
+local _, keymaps = lib.module.run(current_dir .. '/config/keymaps.lua')
 for _, v in ipairs(keymaps) do
 	vim.keymap.set(v.modes, v.keys, v.action)
 end

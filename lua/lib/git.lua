21232d66396a8bf7f5dab0c6b9dfa250ebe76f09
Jan Kot <janymalino@gmail.com>
2025-05-04 01:01:47 +0200

    lib: add git module

---
+++ lua/lib/git.lua
@@ -0,0 +1,11 @@
+local module = {}
+
+function module.clone(url, path, callback, opts)
+	vim.system({ 'git', 'clone', unpack(opts or {}), url, path }, callback)
+end
+
+function module.checkout(repo, rev, callback, opts)
+	vim.system({ 'git', '--work-tree=' .. repo, 'checkout', unpack(opts or {}) }, callback)
+end
+
+return module
--- lua/lib/init.lua
+++ lua/lib/init.lua
@@ -1,8 +1,9 @@
 local module = {
 	debug = require("lib.debug"),
 	filesystem = require("lib.filesystem"),
+	git = require("lib.git"),
 	iterator = require("lib.iterator"),
+	module = require("lib.module"),
 	string = require("lib.string"),
-	module = require("lib.module")
 }
 return module

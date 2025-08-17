73e08e240db53a92d6f7cc219168af667b739009
Jan Kot <janymalino@gmail.com>
2025-08-17 14:01:59 +0200

    add diagnostics configuration

--- init.lua
+++ init.lua
@@ -6,6 +6,23 @@ local lm = require('language-manager')
 
 vim.g.mapleader = ' '
 
+vim.diagnostic.config(
+	{
+		underline = false,
+		virtual_lines = true,
+		update_in_insert = false,
+		severity_sort = true,
+		signs = {
+			text = {
+				[vim.diagnostic.severity.ERROR] = "●",
+				[vim.diagnostic.severity.WARN] = "●",
+				[vim.diagnostic.severity.HINT] = "●",
+				[vim.diagnostic.severity.INFO] = "●",
+			},
+		},
+	}
+)
+
 vim.filetype.add({
 	extension = {
 		hexpat = "hexpat"

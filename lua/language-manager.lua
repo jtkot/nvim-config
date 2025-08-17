3d0a37280e969cbb505c3030978d6f78c087ee5e
Jan Kot <janymalino@gmail.com>
2025-08-17 14:02:58 +0200

    updates to language-manager

--- lua/language-manager.lua
+++ lua/language-manager.lua
@@ -6,7 +6,8 @@ local _module = {}
 _module.install_path = vim.fs.joinpath(vim.fn.stdpath('data'), 'lsps.lua')
 
 function _module.update_definitions()
-	local languages_url = 'https://raw.githubusercontent.com/helix-editor/helix/050e1d9b4797c9ec858bb977cb550f2506a4204b/languages.toml'
+	local languages_url =
+	'https://raw.githubusercontent.com/helix-editor/helix/050e1d9b4797c9ec858bb977cb550f2506a4204b/languages.toml'
 	local languages_toml = curl.fetch(languages_url)
 	local parsed = yq.parse_toml(languages_toml)
 
@@ -24,26 +25,35 @@ function _module.update_definitions()
 		local servers = language['language-servers'] or {}
 		for server in vim.iter(servers) do
 			local server_name = server.name or server
-			if not configs[server_name] then
-				vim.print(server_name)
+			LanguageName = nil
+			for ft in vim.iter(language['file-types']) do
+				if type(ft) == 'string' then
+					LanguageName, _ = vim.filetype.match({ filename = 'test.' .. ft })
+					if LanguageName then
+						table.insert(configs[server_name].filetypes, language.name)
+						break
+					end
+				end
 			end
-			table.insert(configs[server_name].filetypes, language.name)
-			configs[server_name].root_markers = { unpack(language.roots or {}), unpack(configs[server_name].root_markers or {}) }
+			if language.roots then
+				configs[server_name].root_markers = { unpack(language.roots), unpack(configs[server_name].root_markers or
+					{}) }
+			end
 		end
 	end
 	local output_file = io.open(_module.install_path, "w")
 	assert(output_file, "Couldn't open the output file")
-	output_file:write('return ' .. vim.inspect(configs))
+	output_file:write('-- Generated with the :LspUpdate command. DO NOT MODIFY MANUALLY!\nreturn ' .. vim.inspect(configs))
 	output_file:close()
 end
 
 function _module.load_lsp(name, config)
-		vim.lsp.config(name, config)
-		vim.lsp.enable(name)
+	vim.lsp.config(name, config)
+	vim.lsp.enable(name)
 end
 
 function _module.load_lsps()
-	if vim.fn.filereadable(_module.install_path) == 0 then
+	if not vim.uv.fs_stat(_module.install_path) then
 		_module.update_definitions()
 	end
 	local _, lsps = module.run(_module.install_path)

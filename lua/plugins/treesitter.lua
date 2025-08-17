9c4b060c6fe344017af8de7fde1a2fb58fb26ed3
Jan Kot <janymalino@gmail.com>
2025-08-17 14:00:41 +0200

    add tree-sitter parser for hexpat

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,18 +2,31 @@ return {
 	repo = 'nvim-treesitter/nvim-treesitter',
 	branch = 'master',
 	module_name = 'nvim-treesitter.configs',
-	config = {
-			ensure_installed = 'all',
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false
-			},
-			indent = {
-				enable = true
-			},
-			incremental_selection = {
-				enable = true
+	hooks = {
+		pre_load = function()
+			local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+			parser_config.hexpat = {
+				install_info = {
+					url = "https://github.com/NathanSnail/tree-sitter-hexpat.git",
+					files = { "src/parser.c" },
+					generate_requires_npm = false,
+					requires_generate_from_grammar = true,
+				}
 			}
+		end
+	},
+	config = {
+		ensure_installed = 'all',
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false
+		},
+		indent = {
+			enable = true
+		},
+		incremental_selection = {
+			enable = true
+		}
 	}
 }

79970b42b98c265b71b6311c31f95783daeeb9aa
Jan Kot <janymalino@gmail.com>
2025-08-17 13:59:57 +0200

    add telescope plugin

---
+++ lua/plugins/plenary.lua
@@ -0,0 +1,4 @@
+return {
+	repo = 'nvim-lua/plenary.nvim',
+	call_setup = false
+}
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+	repo = 'nvim-telescope/telescope.nvim',
+	branch = '0.1.x',
+	config = {
+		defaults = {
+			layout_strategy = 'vertical',
+			layout_config = {
+				vertical = { width = 0.5 }
+			}
+		}
+	}
+}

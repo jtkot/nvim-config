b2860d16b9ea0317a68889531a18be263a9036dc
Jan Kot <janymalino@gmail.com>
2025-11-09 15:55:18 +0100

    replace mypy with pyrefly

--- lua/config/language-servers.lua
+++ lua/config/language-servers.lua
@@ -1,6 +1,24 @@
 return {
 	["efm-langserver"] = {
 		cmd = { 'efm-langserver' },
-		filetypes = {'meson', 'javascript', 'typescript', 'hexpat'}
+		filetypes = { 'meson', 'javascript', 'typescript', 'hexpat' }
+	},
+	["pyrefly"] = {
+		cmd = { 'pyrefly', 'lsp' },
+		filetypes = { 'python' },
+		root_markers = { "pyrefly.toml", "pyproject.toml", ".git" }
+	},
+	["pylsp"] = {
+		cmd = { 'pylsp' },
+		filetypes = { 'python' },
+		settings = {
+			pylsp = {
+				plugins = {
+					pylsp_mypy = {
+						enabled = false
+					}
+				}
+			}
+		}
 	}
 }

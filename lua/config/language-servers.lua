5c0df32f7706312d8c3d716dfb48a5498679334f
Jan Kot <janymalino@gmail.com>
2025-12-29 15:34:33 +0100

    replace pyrefly lsp with ty

--- lua/config/language-servers.lua
+++ lua/config/language-servers.lua
@@ -3,11 +3,6 @@ return {
 		cmd = { 'efm-langserver' },
 		filetypes = { 'meson', 'javascript', 'typescript', 'hexpat' }
 	},
-	["pyrefly"] = {
-		cmd = { 'pyrefly', 'lsp' },
-		filetypes = { 'python' },
-		root_markers = { "pyrefly.toml", "pyproject.toml", ".git" }
-	},
 	["nixd"] = {
 		cmd = { 'nixd' },
 		filetypes = { 'nix' },
@@ -17,17 +12,4 @@ return {
 			}
 		}
 	},
-	["pylsp"] = {
-		cmd = { 'pylsp' },
-		filetypes = { 'python' },
-		settings = {
-			pylsp = {
-				plugins = {
-					pylsp_mypy = {
-						enabled = false
-					}
-				}
-			}
-		}
-	}
 }
